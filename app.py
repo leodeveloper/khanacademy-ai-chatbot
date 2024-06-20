039b5713c24241f85996ea82b2b3bd86536690cd
Muhammad Suleman <leodeveloper@gmail.com>
2024-06-20 17:57:07 +0400

    add configuration

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
+
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import os
 import streamlit as st
+import sqlite3
 import json
 from datetime import datetime
 from langchain_chroma import Chroma
